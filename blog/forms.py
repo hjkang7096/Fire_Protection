88b4083501239d9056fa5779297afd21605f37a5
hjkang7096 <hjkang7096@naver.com>
2022-09-07 17:44:14 +0900

    blog model changed(add author / title changed)

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ["title", "content"]
--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("<int:pk>/", views.post_detail, name="post_detail"),
     path("post_new/", views.post_new, name="post_new"),
+    path("post_update/<int:pk>/", views.post_update, name="post_update"),
+    path("post_delete/<int:pk>/", views.post_delete, name="post_delete"),
 ]
