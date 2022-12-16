7a78f425d6989242826ec455febae79367dfa39b
hjkang7096 <hjkang7096@naver.com>
2022-12-17 08:31:57 +0900

    do_it_django_setting(도커에 staticfiles 적용)

--- mysite/urls.py
+++ mysite/urls.py
@@ -38,5 +38,8 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:  (Holix settings)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
