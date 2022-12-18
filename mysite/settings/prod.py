afb929eccb0bb3345f3b14a195ae052100333b5f
hjkang7096 <hjkang7096@naver.com>
2022-12-18 23:16:35 +0900

    do_it_django_setting(개발용과 배포용 도커파일 분리)

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -2,22 +2,22 @@ import os
 
 from .common import *
 
-DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "level": "ERROR",
-            "class": "logging.StreamHandler"
-        },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["console"],
-            "level": "ERROR",
-        },
-    },
-}
+# DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+#
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "handlers": {
+#         "console": {
+#             "level": "ERROR",
+#             "class": "logging.StreamHandler"
+#         },
+#     },
+#     "loggers": {
+#         "django": {
+#             "handlers": ["console"],
+#             "level": "ERROR",
+#         },
+#     },
+# }
