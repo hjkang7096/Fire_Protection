c3d5da585cd7009c9f3d33a09d16ed53d463120d
hjkang7096 <hjkang7096@naver.com>
2022-12-20 23:37:50 +0900

    do_it_django_setting(staticfiles)

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
